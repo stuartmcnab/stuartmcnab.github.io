b9bc552b68a1915631ea7cea166fadbcf465cbac
Stuart McNab <contactstuartonline@gmail.com>
2024-02-20 16:01:11 +0000

    Add BMI

--- calculators/body-fat/script.js
+++ calculators/body-fat/script.js
@@ -4,12 +4,14 @@ const calculateBodyFat = () => {
     const neck = parseFloat(document.getElementById('neck').value);
     const weight = parseFloat(document.getElementById('weight').value);
 
-    let bodyFatPercentage = (495 / (1.0324 - 0.19077 * Math.log10(waist - neck) + 0.15456 * Math.log10(height)) - 450).toFixed(0)
+    let bodyFatPercentage = (495 / (1.0324 - 0.19077 * Math.log10(waist - neck) + 0.15456 * Math.log10(height)) - 450)
+
 
     document.getElementById('result').innerHTML = !bodyFatPercentage || bodyFatPercentage === 'NaN' ? 'Please complete all of the fields' : 
-    `Body Fat Percentage (YMCA Formula): ${bodyFatPercentage}%<br>
-    Fat mass: ${((weight * bodyFatPercentage) / 100).toFixed(0)}kg<br>
-    Lean body mass: ${(weight - ((weight * bodyFatPercentage) / 100)).toFixed(0)}kg<br>`;
+    `Body Fat Percentage (YMCA Formula): ${bodyFatPercentage.toFixed(2)}%<br>
+    Fat mass: ${((weight * bodyFatPercentage) / 100).toFixed(2)}kg<br>
+    Lean body mass: ${(weight - ((weight * bodyFatPercentage) / 100)).toFixed(2)}kg<br>
+    BMI: ${(weight/ (height/100 * height/100)).toFixed(2)}`;
 }
 
 
